576bb84685e154f324e272cacd3f2538c8063f6c
Lucas Magno <lmoabreu@gmail.com>
2021-04-21 12:13:30 -0300

    api request (SSR example)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import React, { useEffect } from "react";
+import { Header } from "../components/Header";
 
-export default function Home() {
-  useEffect(() => {
-    fetch('http://localhost:3333/episodes')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
+export default function Home(props) {
+  console.log(props.episodes);
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data
+    }
+  }
 }
