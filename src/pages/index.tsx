bdac96a4b8ea23af7068a3ef7d5743b3698941a6
Lucas Magno <lmoabreu@gmail.com>
2021-04-21 14:42:59 -0300

    add axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
-import React, { useEffect } from "react";
-import { Header } from "../components/Header";
+import React from "react";
+import { GetStaticProps } from 'next';
+import { api } from "../services/api";
 
-export default function Home(props) {
-  console.log(props.episodes);
+type Episode = {
+  id: string;
+  title: string;
+  member: string;
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
@@ -11,9 +21,14 @@ export default function Home(props) {
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
 
   return {
     props: {
